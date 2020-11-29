5a908a76f45d678d58e92efb84d008e88b250ed8
Hi-yano <73208511+Hi-yano@users.noreply.github.com>
2020-11-29 12:46:51 +0900

    ルーティングの記述

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,4 +1,21 @@
 class AreasController < ApplicationController
   def new
+    @area = Area.new
+  end
+
+  def create
+    area = Area.new(area_params)
+    if area.valid?
+      area.save
+      redirect_to new_area_condition_path
+    else
+      redirect_to new_area_path
+    end
+  end
+
+  private
+
+  def area_params
+    params.require(:area).permit(:dive_day, :entry_id, :region).merge(user_id: current_user.id)
   end
 end
