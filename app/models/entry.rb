27b584ace6a585ae3196e624a057ac01abae0b77
Hi-yano <73208511+Hi-yano@users.noreply.github.com>
2020-11-29 12:39:39 +0900

    area機能の実装

---
+++ app/models/entry.rb
@@ -0,0 +1,12 @@
+class Entry < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: 'ボートダイブ' },
+    { id: 3, name: 'ビーチダイブ' },
+    { id: 4, name: 'ドリフトダイブ' },
+    { id: 5, name: 'ナイトダイブ' },
+  ]
+
+  include ActiveHash::Associations
+  has_many :areas
+end
