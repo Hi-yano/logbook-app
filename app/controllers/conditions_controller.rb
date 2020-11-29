61e629d8aca816c4031582fbc3efdfa452544e4a
Hi-yano <73208511+Hi-yano@users.noreply.github.com>
2020-11-29 13:03:13 +0900

    コントローラー・ビューファイルの設置

---
+++ app/controllers/conditions_controller.rb
@@ -0,0 +1,4 @@
+class ConditionsController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'conditions/new'
   devise_for :users
   resources :users, only: [:edit, :update, :show]
   resources :areas, only: [:new, :create]
