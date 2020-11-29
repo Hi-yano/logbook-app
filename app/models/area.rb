422c1b87ea52cd1ee29044704fd9a4e6ec88f2df
Hi-yano <73208511+Hi-yano@users.noreply.github.com>
2020-11-29 13:07:30 +0900

    アソシエーションの設定

--- app/models/area.rb
+++ app/models/area.rb
@@ -2,4 +2,5 @@ class Area < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :entry
   belongs_to :user
+  belongs_to :condition
 end
--- app/models/condition.rb
+++ app/models/condition.rb
@@ -2,4 +2,5 @@ class Condition < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :tide
   belongs_to :user
+  belongs_to :area
 end
