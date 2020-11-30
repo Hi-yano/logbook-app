8d7bae7aee7b490c4e62a09bd29020ab734a448a
Hi-yano <73208511+Hi-yano@users.noreply.github.com>
2020-11-30 14:10:14 +0900

    logbook保存機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,7 @@ class ItemsController < ApplicationController
     item = Item.new(item_params)
     if item.valid?  
       item.save
-      redirect_to new_log_path
+      redirect_to new_area_logbook_path
     else
       render :new
     end
--- app/controllers/logbooks_controller.rb
+++ app/controllers/logbooks_controller.rb
@@ -1,7 +1,24 @@
 class LogbooksController < ApplicationController
+  
   def index
   end
 
   def new
+    @logbook = Logbook.new
+  end
+
+  def create
+    logbook = Logbook.new(logbook_params)
+    if logbook.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def logbook_params
+    params.require(:logbook).permit(:image, :point, :start_air, :finish_air, :max_depth, :ave_depth, :entry_time, :exit_time, :count, :text).merge(area_id: params[:area_id])
   end
 end
