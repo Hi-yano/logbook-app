a88d5ffa6b20785ac4ed1354284ff000d47667b2
Hi-yano <73208511+Hi-yano@users.noreply.github.com>
2020-12-04 17:20:59 +0900

    画像とツイートを表示

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,20 @@
 class TweetsController < ApplicationController
-
-  def index
-    @tweets = Tweet.all.order("created_at DESK")
-  end
-
   def new
+    @tweet = Tweet.new
   end
 
   def create
+    tweet = Tweet.new(tweet_params)
+    if tweet.save
+      redirect_to root_path
+    else
+      redirect_to new_tweet_path
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:image, :text).merge(user_id: current_user.id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_100736) do
+ActiveRecord::Schema.define(version: 2020_12_04_054205) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -82,6 +82,13 @@ ActiveRecord::Schema.define(version: 2020_12_01_100736) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["area_id"], name: "index_logbooks_on_area_id"
+  end
+
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "text"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
