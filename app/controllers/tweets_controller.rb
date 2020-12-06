56cef30cc4323108c6c9a9c741ca94e66b471c55
Hi-yano <73208511+Hi-yano@users.noreply.github.com>
2020-12-06 19:24:45 +0900

    ツイート編集機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -12,6 +12,19 @@ class TweetsController < ApplicationController
     end
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to root_path
+    else
+      redirect_to edit_tweet_path
+    end
+  end
+
   private
 
   def tweet_params
