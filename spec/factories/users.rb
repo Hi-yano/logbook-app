5b64f294b555c2e38dd02d22c64b33534e3a8796
Hi-yano <73208511+Hi-yano@users.noreply.github.com>
2020-12-15 09:40:13 +0900

    テストコードの記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.last_name }
     email { Faker::Internet.free_email }
-    password { Faker::Internet.password(min_length: 6) }
+    password = "aaa111"
+    password { password }
     password_confirmation { password }
   end
 end
